d1a82c01c4260bdc86a78cd843ce7eee362a3ae3
Robert Sehr <robert.sehr@intranda.com>
2025-05-23 14:19:22 +0200

    export arche data to viewer

--- module-base/src/main/java/de/intranda/goobi/plugins/ArcheToViewerExportPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/ArcheToViewerExportPlugin.java
@@ -1,29 +1,37 @@
 package de.intranda.goobi.plugins;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.goobi.api.ArcheConfiguration;
+import org.goobi.api.rest.ArcheAPI;
 import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IExportPlugin;
 import org.goobi.production.plugin.interfaces.IPlugin;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
-import de.sub.goobi.config.ConfigPlugins;
-import de.sub.goobi.export.dms.ExportDms;
-import de.sub.goobi.helper.VariableReplacer;
+import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.export.download.ExportMets;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.XmlTools;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.helper.exceptions.ExportFileException;
 import de.sub.goobi.helper.exceptions.SwapException;
 import de.sub.goobi.helper.exceptions.UghHelperException;
+import jakarta.ws.rs.client.Client;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
-import ugh.dl.DigitalDocument;
-import ugh.dl.Fileformat;
-import ugh.dl.Prefs;
 import ugh.exceptions.DocStructHasNoTypeException;
 import ugh.exceptions.MetadataTypeNotAllowedException;
 import ugh.exceptions.PreferencesException;
@@ -32,8 +40,9 @@ import ugh.exceptions.TypeNotAllowedForParentException;
 import ugh.exceptions.WriteException;
 
 @PluginImplementation
-@Log4j2
 public class ArcheToViewerExportPlugin implements IExportPlugin, IPlugin {
+
+    private static final long serialVersionUID = 5900921810436024070L;
 
     @Getter
     private String title = "intranda_export_arche_to_viewer";
@@ -46,6 +55,12 @@ public class ArcheToViewerExportPlugin implements IExportPlugin, IPlugin {
     @Getter
     private List<String> problems;
 
+    private ArcheConfiguration archeConfiguration;
+
+    private static final Namespace metsNamespace = Namespace.getNamespace("mets", "http://www.loc.gov/METS/");
+    private static final Namespace modsNamespace = Namespace.getNamespace("mods", "http://www.loc.gov/mods/v3");
+    private static final Namespace xlinkNamespace = Namespace.getNamespace("xlink", "http://www.w3.org/1999/xlink");
+
     @Override
     public void setExportFulltext(boolean arg0) {
     }
@@ -56,47 +71,141 @@ public class ArcheToViewerExportPlugin implements IExportPlugin, IPlugin {
 
     @Override
     public boolean startExport(Process process) throws IOException, InterruptedException, DocStructHasNoTypeException, PreferencesException,
-    WriteException, MetadataTypeNotAllowedException, ExportFileException, UghHelperException, ReadException, SwapException, DAOException,
-    TypeNotAllowedForParentException {
+            WriteException, MetadataTypeNotAllowedException, ExportFileException, UghHelperException, ReadException, SwapException, DAOException,
+            TypeNotAllowedForParentException {
         String benutzerHome = process.getProjekt().getDmsImportImagesPath();
         return startExport(process, benutzerHome);
     }
 
     @Override
     public boolean startExport(Process process, String destination) throws IOException, InterruptedException, DocStructHasNoTypeException,
-    PreferencesException, WriteException, MetadataTypeNotAllowedException, ExportFileException, UghHelperException, ReadException,
-    SwapException, DAOException, TypeNotAllowedForParentException {
+            PreferencesException, WriteException, MetadataTypeNotAllowedException, ExportFileException, UghHelperException, ReadException,
+            SwapException, DAOException, TypeNotAllowedForParentException {
+
         problems = new ArrayList<>();
 
-        // read information from config file
-        String test = ConfigPlugins.getPluginConfig(title).getString("value", "");
-        System.out.println("value from configuration file: " + test);
+        // first validate if project, process, files are ingested into arche instance
+        archeConfiguration = new ArcheConfiguration("intranda_administration_arche_project_export");
+        String idPrefix = archeConfiguration.getIdentifierPrefix();
+        String projectIdentifier = null;
+        String processIdentifier = null;
+        String mediaFolderIdentifier = null;
+        String ocrFolderIdentifier = null;
+        try (Client client = ArcheAPI.getClient(archeConfiguration.getArcheUserName(), archeConfiguration.getArchePassword())) {
 
-        // read mets file to test if it is readable
-        try {
-            Prefs prefs = process.getRegelsatz().getPreferences();
-            Fileformat ff = null;
-            ff = process.readMetadataFile();
-            DigitalDocument dd = ff.getDigitalDocument();
-            VariableReplacer replacer = new VariableReplacer(dd, prefs, process, null);
-        } catch (ReadException | PreferencesException | IOException | SwapException e) {
-            log.error(e);
-            problems.add("Cannot read metadata file.");
+            projectIdentifier = idPrefix + process.getProjekt().getTitel();
+
+            String resourceUri = ArcheAPI.findResourceURI(client, archeConfiguration.getArcheApiUrl(), projectIdentifier);
+            if (StringUtils.isBlank(resourceUri)) {
+                problems.add("Project was not exported to Arche.");
+                return false;
+            }
+
+            processIdentifier = projectIdentifier + "/" + process.getTitel();
+
+            resourceUri = ArcheAPI.findResourceURI(client, archeConfiguration.getArcheApiUrl(), processIdentifier);
+            if (StringUtils.isBlank(resourceUri)) {
+                problems.add("Process was not exported to Arche.");
+                return false;
+            }
+
+            mediaFolderIdentifier = processIdentifier + "/" + process.getTitel() + "_media/";
+            ocrFolderIdentifier = processIdentifier + "/" + process.getTitel() + "_ocr/";
+
+            for (String filename : StorageProvider.getInstance().list(process.getImagesTifDirectory(false))) {
+                String imageIdentifier = mediaFolderIdentifier + filename;
+                resourceUri = ArcheAPI.findResourceURI(client, archeConfiguration.getArcheApiUrl(), imageIdentifier);
+                if (StringUtils.isBlank(resourceUri)) {
+                    problems.add("Image " + filename + " was not exported to Arche.");
+                    return false;
+                }
+
+            }
+
+            for (String filename : StorageProvider.getInstance().list(process.getOcrAltoDirectory())) {
+                String ocrIdentifier = ocrFolderIdentifier + filename;
+                resourceUri = ArcheAPI.findResourceURI(client, archeConfiguration.getArcheApiUrl(), ocrIdentifier);
+                if (StringUtils.isBlank(resourceUri)) {
+                    problems.add("ALTO file " + filename + " was not exported to Arche.");
+                    return false;
+                }
+            }
+        }
+
+        // regular export into temp folder
+        ExportMets export = new ExportMets();
+        boolean success = export.startExport(process, ConfigurationHelper.getInstance().getTemporaryFolder());
+        Path metsfile = Paths.get(ConfigurationHelper.getInstance().getTemporaryFolder(), process.getTitel() + "_mets.xml");
+        Path anchorfile = Paths.get(ConfigurationHelper.getInstance().getTemporaryFolder(), process.getTitel() + "_anchor_mets.xml");
+        if (!success) {
+            problems.addAll(export.getProblems());
+            // remove temp files
+            if (StorageProvider.getInstance().isFileExists(metsfile)) {
+                StorageProvider.getInstance().deleteFile(metsfile);
+            }
+            if (StorageProvider.getInstance().isFileExists(anchorfile)) {
+                StorageProvider.getInstance().deleteFile(anchorfile);
+            }
+
             return false;
         }
 
-        // do a regular export here
-        IExportPlugin export = new ExportDms();
-        export.setExportFulltext(true);
-        export.setExportImages(true);
+        // read exported file
+        Document doc = XmlTools.readDocumentFromFile(metsfile);
+        Element mets = doc.getRootElement();
 
-        // execute the export and check the success
-        boolean success = export.startExport(process);
-        if (!success) {
-            log.error("Export aborted for process with ID " + process.getId());
-        } else {
-            log.info("Export executed for process with ID " + process.getId());
+        // get main metadata section
+        List<Element> dmdSecs = mets.getChildren("dmdSec", metsNamespace);
+        Element dmdSec = dmdSecs.get(0);
+        Element mods = dmdSec.getChild("mdWrap", metsNamespace).getChild("xmlData", metsNamespace).getChild("mods", modsNamespace);
+
+        // add process resource url as mods:identifier
+        Element identifier = new Element("identifier", modsNamespace);
+        identifier.setText(processIdentifier);
+        identifier.setAttribute("type", "arche");
+        mods.addContent(identifier);
+
+        // replace file groups with arche iiif urls
+        Element fileSec = mets.getChild("fileSec", metsNamespace);
+
+        // find filegroups for presentation, thumbs,  alto/fulltext
+        for (Element fileGrp : fileSec.getChildren("fileGrp", metsNamespace)) {
+            String name = fileGrp.getAttributeValue("USE");
+            List<Element> filesInGrp = fileGrp.getChildren("file", metsNamespace);
+            for (Element fileElement : filesInGrp) {
+                Element flocat = fileElement.getChild("FLocat", metsNamespace);
+                flocat.setAttribute("type", "simple", xlinkNamespace);
+                String filename = Paths.get(flocat.getAttributeValue("href", xlinkNamespace)).getFileName().toString();
+
+                switch (name.toLowerCase()) {
+                    case "presentation", "default":
+                        flocat.setAttribute("href", mediaFolderIdentifier + filename + "?format=image%2Fjpeg", xlinkNamespace);
+                        fileElement.setAttribute("MIMETYPE", "image/jpeg");
+                        break;
+                    case "thumbs", "thumbnail", "thumbnails":
+                        flocat.setAttribute("href", mediaFolderIdentifier + filename + "?format=thumbnail", xlinkNamespace);
+                        fileElement.setAttribute("MIMETYPE", "image/png");
+                        break;
+                    case "alto", "fulltext":
+                        flocat.setAttribute("href", ocrFolderIdentifier + filename, xlinkNamespace);
+                        fileElement.setAttribute("MIMETYPE", "application/xml");
+                        break;
+                }
+
+            }
+        }
+
+        // save xml file
+        XMLOutputter xmlOut = new XMLOutputter(Format.getPrettyFormat());
+        xmlOut.output(doc, StorageProvider.getInstance().newOutputStream(metsfile));
+
+        // move results to destination
+        StorageProvider.getInstance().move(metsfile, Paths.get(destination, metsfile.getFileName().toString()));
+
+        if (StorageProvider.getInstance().isFileExists(anchorfile)) {
+            StorageProvider.getInstance().move(anchorfile, Paths.get(destination, anchorfile.getFileName().toString()));
         }
         return success;
     }
+
 }
